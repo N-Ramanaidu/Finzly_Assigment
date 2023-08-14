63e8afe527ff05aed6fcedfd2556656b37dd6c50
Nallajonnala Ramanaidu <88706613+N-Ramanaidu@users.noreply.github.com>
2023-08-14 18:45:46 +0530

    Add files via upload

---
+++ While/src/Main.java
@@ -0,0 +1,387 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    	System.out.println("---------------------------------------");
+		System.out.println("Programs on While Loop ....!!!!!");
+		System.out.println("---------------------------------------");
+	
+		// 1) Write a Program To print 1 to 25 nos.	
+		
+		System.out.println("1. Print number from 1 to 25 : ");
+        int i = 0;
+        while (i <= 25) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        //2) Write a Program To print 25 to 1 nos.
+		
+		System.out.println("2. Print 1 to 25 nos :");
+        i = 25;
+        while (i > 0) {
+            System.out.print(i + " ");
+            i--;
+        }
+        System.out.println();
+
+        // 3) Write a Program To print 1 to 100 Odd nos.
+		
+		System.out.println("3. Print 1 to 100 Odd nos :");
+        i = 0;
+        while (i <= 100) {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        // 4) Write a Program To print 1 to 100 even nos.
+
+		System.out.println("4. Print 1 to 100 even nos :");
+        i = 0;
+        while (i <= 100) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        // 5) Write a Program To print sum of 1 to 50 Odd nos.
+		
+		System.out.println("5. Print 1 to 50 odd nos :");
+        i = 0;
+        int sum = 0;
+        while (i <= 50) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+            i++;
+        }
+        System.out.print(sum);
+        System.out.println();
+
+        // 6) Write a Program To print sum of 1 to 50 EVEN nos.
+		
+		System.out.println("6. Print 1 to 50 even nos :");
+        i = 0;
+        sum = 0;
+        while (i <= 50) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+            i++;
+        }
+        System.out.print(sum);
+        System.out.println();
+
+        // 7) Write a Program To print -45 to +45 nos.
+		
+		System.out.println("7. Print -45 to +45 nos :");
+        i = -45;
+        while (i <= 45) {
+
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        // 8) Write a Program To print 50 to 100 nos.
+		
+		System.out.println("8. Print 50 to 100 nos :");
+        i = 50;
+        while (i <= 100) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        // 9) Write a Program To print sum of odd and even no.
+		
+		System.out.println("9. Print sum of odd and even no :");
+        i = 0;
+        int odd = 0, even = 0;
+        while (i <= 100) {
+            if (i % 2 != 0)
+                odd += i;
+            else even += i;
+            i++;
+        }
+        System.out.print(odd + " " + even);
+        System.out.println();
+
+        // 10) Write a Program To print even and odd No
+		
+		System.out.println("10. Print even and odd no :");
+        i = 0;
+        System.out.print("even : ");
+        while (i <= 100) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+            i++;
+
+        }
+        System.out.println();
+        i = 0;
+        System.out.print("odd : ");
+        while (i <= 100) {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+            i++;
+
+        }
+        System.out.println();
+
+        // 11) Write a Program To print 1 to 100 no.
+		
+		System.out.println("11. Print 1 to 100 no :");
+
+        i = 1;
+        while (i <= 100) {
+
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        // 12) Write a Program To print 100 to 1 no.
+		
+		System.out.println("12. Print 100 to 1 no :");
+        i=100;
+        while (i>=1){
+
+            System.out.print(i+" ");
+            i--;
+        }
+        System.out.println();
+
+        // 13) Write a Program To print 30 to 50 no.
+		
+		System.out.println("13. Print 30 to 50 no :");
+        i=30;
+        while (i<=50){
+
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 14) Write a Program To print count of even No 1 to 25 no.
+		
+		System.out.println("14. Print count of even No 1 to 25 no :");
+        i=1;
+        while (i<=25){
+            if(i%2==0)
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+
+        // 15) Write a Program To print count of odd No 1 to 25 no.
+		
+		System.out.println("15. Print count of odd No 1 to 25 no :");
+        i=1;
+        while (i<=25){
+            if(i%2!=0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        //////   Series
+        
+        System.out.println();
+        System.out.println("---------SERIES-----------------");
+        System.out.println();
+
+
+        // 16) Generate the Series... 2 4 6 8 10 12 14 16 18 20.
+		
+		System.out.println("16. Print.... 2 4 6 8 10 12 14 16 18 20 :");
+		
+        i=1;
+        while (i<=20){
+            if(i%2==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 17) Generate the Series... 9 18 27 36 45 54 63 72 81 90. 
+		
+		System.out.println("17. Print.... 9 18 27 36 45 54 63 72 81 90 :");
+        i=1;
+        while (i<=20){
+            if(i%3==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 18) Generate the Series... 1 -2 3 -4 5 -6 7 -8 9 -10.
+		
+		System.out.println("18. Print.... 1 -2 3 -4 5 -6 7 -8 9 -10 :");
+        i=1;
+        while (i<=20){
+            if(i%2==0)
+                System.out.print(-i+" ");
+            else System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 19) Generate the Series... 5 10 15 20 25 30 35 40 45 50.
+		
+		System.out.println("19. Print.... 5 10 15 20 25 30 35 40 45 50 :");
+
+        i=1;
+        while (i<=20){
+            if(i%5==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 20) Generate the Series... 1 10 100 1000.
+		
+		System.out.println("20. Print.... 1 10 100 1000 :");
+        i=1;
+        while (i<=1000){
+            i=i*10;
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        // 21) Generate the Series... 1  3  6  10  15  21  28  36  45
+		
+		System.out.println("21. Print.... 1  3  6  10  15  21  28  36  45 :");
+        i=1;
+        sum=0;
+        while (i<10){
+            sum=sum+i;
+            System.out.print(sum+" ");
+            i++;
+        }
+        System.out.println();
+
+        //22) Generate the Series... 8 16 24 32 40 48 56 64 72 80.
+		
+		System.out.println("22. Print.... 8 16 24 32 40 48 56 64 72 80 :");
+
+        i=1;
+        while (i<=80){
+            if(i%8==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 23) Generate the Series... 0 1 1 2 3 5 8 13 21.
+		
+		System.out.println("23. Print.... 0 1 1 2 3 5 8 13 21 :");
+        i=2;
+        int n1=0,n2=1,n3;
+        System.out.print(n1+" "+ n2 +" ");
+        while (i<9){
+            n3=n1+n2;
+            System.out.print(n3+" ");
+            n1=n2;
+            n2=n3;
+            i++;
+        }
+        System.out.println();
+
+        // 24) Generate the series... 1 4 9 16 25 36 49 64 81 100.
+		
+		System.out.println("24. Print.... 1 4 9 16 25 36 49 64 81 100 :");
+        i=1;
+        while (i<=10){
+            int d=i*i;
+                System.out.print(d+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 25) Generate the series... 3 6 9 12 15 18 21 24 27 30.
+		
+		System.out.println("25. Print.... 3 6 9 12 15 18 21 24 27 30 :");
+        i=1;
+        while (i<=30){
+            if(i%3==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 26) Generate the Series... 7 14 21 28 35 42 49 56 63 70. 
+		
+		System.out.println("26. Print.... 7 14 21 28 35 42 49 56 63 70 :");
+        i=1;
+        while (i<=70){
+            if(i%7==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 27) Generate the Series... 4 8 12 16 20 24 28 32 36 40.
+		
+		System.out.println("27. Print....4 8 12 16 20 24 28 32 36 40 :");
+        i=1;
+        i=1;
+        while (i<=40){
+            if(i%4==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 28) Generate the Series... 10 20 30 40 50 60 70 80 90 100.
+		
+		System.out.println("28. Print....10 20 30 40 50 60 70 80 90 100 :");
+        i=1;
+        while (i<=100){
+            if(i%10==0)
+                System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        // 29) Generate the Series... 1 2 3 4 5 4 3 2 1. 
+		
+		System.out.println("29. Print....  1 2 3 4 5 4 3 2 1 :");
+        i=1;
+        while (i<5){
+
+            System.out.print(i+" ");
+            i++;
+        }
+
+        while (i>0){
+         System.out.print(i+" ");
+         i--;
+        
+        }
+
+         System.out.println();
+        // 30) Generate the Series... 6 12 18 24 30 36 42 48 54 60.
+		
+		System.out.println("30. Print....  6 12 18 24 30 36 42 48 54 60 :");
+        i=1;
+        while (i<=60){
+            if(i%6==0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+        System.out.println(" ");
+		System.out.println("---------------------------------------");
+		System.out.println("While Loop Practice Completed....!!!!!");
+		System.out.println("---------------------------------------");
+    }
+		
+}
